2c68d3ef11d8a182e984434e9e6986af6e5060e9
ElifVerdaCodar <codarelifverda@gmail.com>
2021-06-19 12:21:54 +0300

    dökümantasyon eklendi

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
+// OKU: https://www.w3schools.com/js/js_variables.asp
 var dogruTahminler = 0;
 var yanlisTahminler = 0;
 
+// OKU: https://www.w3schools.com/js/js_loop_for.asp
 for (var sayi = 0; sayi < 10; sayi++) {
   //0 ile 10 arasinda rastgele sayı
+  // OKU: https://www.w3schools.com/js/js_math.asp - https://www.w3schools.com/js/js_random.asp
   var rastgeleSayi = Math.round(Math.random() * 10);
   //alert(rastgeleSayi);
 
   //kullanici tahmin girisi
-  var kullaniciTahminNumara = Number(prompt("lutfentahmininizigiriniz", "0"));
+  var kullaniciTahminNumara = Number(prompt("Lütfen tahmininizi giriniz", "0"));
 
   //karşılaştırma
+  // OKU: https://www.w3schools.com/js/js_comparisons.asp
   if (kullaniciTahminNumara == rastgeleSayi) {
+    // OKU: https://www.w3schools.com/js/js_assignment.asp
     dogruTahminler = dogruTahminler + 1;
   } else {
     yanlisTahminler = yanlisTahminler + 1;
@@ -26,3 +31,5 @@ Yanlış Tahminler:${yanlisTahminler}
 `;
   alert(sonuc);
 }
+
+// TODO: https://www.w3schools.com/js/js_functions.asp
