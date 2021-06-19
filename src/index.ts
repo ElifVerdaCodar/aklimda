cee6529e0e2d2fa9dbc52bace3600f811e0d07e2
ElifVerdaCodar <codarelifverda@gmail.com>
2021-06-19 13:05:28 +0300

    arayüz oluşturuldu

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,44 @@
-// OKU: https://www.w3schools.com/js/js_variables.asp
-var dogruTahminler = 0;
-var yanlisTahminler = 0;
+// TODO: https://www.w3schools.com/js/js_functions.asp
+function oyunuBaslat(adim) {
+  // OKU: https://www.w3schools.com/js/js_variables.asp
+  var dogruTahminler = 0;
+  var yanlisTahminler = 0;
 
-// OKU: https://www.w3schools.com/js/js_loop_for.asp
-for (var sayi = 0; sayi < 10; sayi++) {
-  //0 ile 10 arasinda rastgele sayı
-  // OKU: https://www.w3schools.com/js/js_math.asp - https://www.w3schools.com/js/js_random.asp
-  var rastgeleSayi = Math.round(Math.random() * 10);
-  //alert(rastgeleSayi);
+  // OKU: https://www.w3schools.com/js/js_loop_for.asp
+  for (var sayi = 0; sayi < adim; sayi++) {
+    //0 ile 10 arasinda rastgele sayı
+    // OKU: https://www.w3schools.com/js/js_math.asp - https://www.w3schools.com/js/js_random.asp
+    var rastgeleSayi = Math.round(Math.random() * 10);
+    //alert(rastgeleSayi);
 
-  //kullanici tahmin girisi
-  var kullaniciTahminNumara = Number(prompt("Lütfen tahmininizi giriniz", "0"));
+    //kullanici tahmin girisi
+    var kullaniciTahminNumara = Number(
+      prompt("Lütfen tahmininizi giriniz", "0")
+    );
 
-  //karşılaştırma
-  // OKU: https://www.w3schools.com/js/js_comparisons.asp
-  if (kullaniciTahminNumara == rastgeleSayi) {
-    // OKU: https://www.w3schools.com/js/js_assignment.asp
-    dogruTahminler = dogruTahminler + 1;
-  } else {
-    yanlisTahminler = yanlisTahminler + 1;
-  }
+    //karşılaştırma
+    // OKU: https://www.w3schools.com/js/js_comparisons.asp
+    if (kullaniciTahminNumara == rastgeleSayi) {
+      // OKU: https://www.w3schools.com/js/js_assignment.asp
+      dogruTahminler = dogruTahminler + 1;
+    } else {
+      yanlisTahminler = yanlisTahminler + 1;
+    }
 
-  //sonucu göster
-  var sonuc = `
+    //sonucu göster
+    var sonuc = `
 Deneme:${sayi + 1}
 Tutulan Sayı:${rastgeleSayi}
 Girilen Sayı:${kullaniciTahminNumara}
 Doğru Tahminler:${dogruTahminler}
 Yanlış Tahminler:${yanlisTahminler}
 `;
-  alert(sonuc);
+    alert(sonuc);
+  }
 }
 
-// TODO: https://www.w3schools.com/js/js_functions.asp
+var btnBaslat = document.getElementById("btnBaslat");
+btnBaslat.addEventListener("click", function () {
+  var adim = (document.getElementById("txtAdim") as HTMLInputElement).value;
+  oyunuBaslat(Number(adim));
+});
